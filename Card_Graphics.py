b7385c31888ca9ae68fb16879087e5081b3a62ae
neonate11 <nathan.k.vowinkel.mil@army.mil>
2024-05-18 17:30:17 -0400

    worked on card graphics file

--- Card_Graphics.py
+++ Card_Graphics.py
@@ -25,306 +25,282 @@ def make_deck():
     return deck
 
 def return_card(card,double):
-   rank = card.split(' ')[0]
-   suit = card.split(' ')[1]
-   card_graphic = [\
-      '╭─────────╮',\
-      '│error    │',\
-      '│    error│',\
-      '│error    │',\
-      '│    error│',\
-      '│error    │',\
+   rank,suit = card.split()
+
+   card_graphics = {
+      '2': {
+         'not_double': [
+            '╭─────────╮',
+            f'│2   {suit}    │',
+            '│         │',
+            '│         │',
+            '│         │',
+            f'│    {suit}   2│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│           2 │',
+           f'│ {suit}         {suit} │',
+            '│ 2           │',
+            '╰─────────────╯']},
+      '3': {
+         'not_double': [
+            '╭─────────╮',
+            f'│3   {suit}    │',
+            '│         │',
+            f'│    {suit}    │',
+            '│         │',
+            f'│    {suit}   3│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│           3 │',
+            f'│ {suit}    {suit}    {suit} │',
+            '│ 3           │',
+            '╰─────────────╯']},
+      '4': {
+         'not_double': [
+            '╭─────────╮',
+            f'│4 {suit}   {suit}  │',
+            '│         │',
+            '│         │',
+            '│         │',
+            f'│  {suit}   {suit} 4│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│           3 │',
+            f'│ {suit}    {suit}    {suit} │',
+            '│ 3           │',
+            '╰─────────────╯']},
+      '5': {
+         'not_double': [
+            '╭─────────╮',
+            f'│5 {suit}   {suit}  │',
+            '│         │',
+            f'│    {suit}    │',
+            '│         │',
+            f'│  {suit}   {suit} 5│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│   {suit}     {suit} 5 │',
+            f'│      {suit}      │',
+            f'│ 5 {suit}     {suit}   │',
+            '╰─────────────╯']},
+      '6': {
+         'not_double': [
+            '╭─────────╮',
+            f'│6 {suit}   {suit}  │',
+            '│         │',
+            f'│  {suit}   {suit}  │',
+            '│         │',
+            f'│  {suit}   {suit} 6│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│   {suit}  {suit}  {suit} 6 │',
+            '│             │',
+            f'│ 6 {suit}  {suit}  {suit}   │',
+            '╰─────────────╯']},
+      '7': {
+         'not_double': [
+            '╭─────────╮',
+            f'│7 {suit}   {suit}  │',
+            f'│    {suit}    │',
+            f'│  {suit}   {suit}  │',
+            '│         │',
+            f'│  {suit}   {suit} 7│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│   {suit}  {suit}  {suit} 7 │',
+            f'│       {suit}     │',
+            f'│ 7 {suit}  {suit}  {suit}   │',
+            '╰─────────────╯']},
+      '8': {
+         'not_double': [
+            '╭─────────╮',
+            f'│8 {suit}   {suit}  │',
+            f'│    {suit}    │',
+            f'│  {suit}   {suit}  │',
+            f'│    {suit}    │',
+            f'│  {suit}   {suit} 8│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│   {suit}  {suit}  {suit} 8 │',
+            f'│    {suit}   {suit}    │',
+            f'│ 8 {suit}  {suit}  {suit}   │',
+            '╰─────────────╯']},
+      '9': {
+         'not_double': [
+            '╭─────────╮',
+            f'│9 {suit}   {suit}  │',
+            f'│  {suit}   {suit}  │',
+            f'│    {suit}    │',
+            f'│  {suit}   {suit}  │',
+            f'│  {suit}   {suit} 9│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│   {suit} {suit} {suit} {suit} 9 │',
+            f'│      {suit}      │',
+            f'│ 9 {suit} {suit} {suit} {suit}   │',
+            '╰─────────────╯']},
+      '10': {
+         'not_double': [
+            '╭─────────╮',
+            f'│10 {suit} {suit}   │',
+            f'│  {suit} {suit} {suit}  │',
+            '│         │',
+            f'│  {suit} {suit} {suit}  │',
+            f'│   {suit} {suit} 10│',
+            '╰─────────╯'],
+         'double': [
+             '╭─────────────╮',
+            f'│   {suit} {suit} {suit} {suit} 10│',
+            f'│     {suit} {suit}     │',
+            f'│10 {suit} {suit} {suit} {suit}   │',
+            '╰─────────────╯']},
+      'face': {
+         'not_double': [
+            '╭─────────╮',
+            f'│{rank} ▓▓▒▒▓▓ │',
+            f'│{suit} ░░░░░░ │',
+            '│ ▒░░░░░▒ │',
+            f'│ ░░░░░░ {suit}│',
+            f'│ ▓▓▒▒▓▓ {rank}│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            f'│  ▓░░▒░░░▓ {suit}{rank}│',
+            '│  ▒▒░░░░░▒▒  │',
+            f'│{rank}{suit} ▓░░░▒░░▓  │',
+            '╰─────────────╯']},
+      'A ♥': {
+         'not_double': [
+            '╭─────────╮',
+            '│A  _ _   │',
+            '│♥ ( V )  │',
+            '│   \ /   │',
+            '│    V   ♥│',
+            '│        A│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│      ⌢  ♥ A │',
+            '│    ≺   ⧼    │',
+            '│ A ♥  ⌣      │',
+            '╰─────────────╯']},
+      'A ♦': {
+         'not_double': [
+            '╭─────────╮',
+            '│A   ^    │',
+            '│♦ /   \  │',
+            '│  \   /  │'
+            '│    v   ♦│'
+            '│        A│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│      ^  ♦ A │',
+            '│   <     >   │',
+            '│ A ♦  ⌵      │',
+            '╰─────────────╯']},
+      'A ♠': {
+         'not_double': [
+            '╭─────────╮',
+            '│A   ߍ    │',
+            '│♠  / \   │',
+            '│  (_._)  │',
+            '│    |   ♠│',
+            '│        A│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│      ⌢  ♠ A │',
+            '│    ─│   >   │',
+            '│ A ♠  ⌣      │',
+            '╰─────────────╯']},
+      'A ♣': {
+         'not_double': [
+            '╭─────────╮',
+            '│A   _    │',
+            '│♣  ( )   │',
+            '│  (_\'_)  │',
+            '│    |   ♣│',
+            '│        A│',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│         ♣ A │',
+            '│    ─8o      │',
+            '│ A ♣         │',
+            '╰─────────────╯']},
+      'back side': {
+         'not_double': [
+            '╭─────────╮',
+            '│⚜ ⚜ ⚜ ⚜  │',
+            '│ ⚜ ⚜ ⚜ ⚜ │',
+            '│⚜ ⚜ ⚜ ⚜  │',
+            '│ ⚜ ⚜ ⚜ ⚜ │',
+            '│⚜ ⚜ ⚜ ⚜  │',
+            '╰─────────╯'],
+         'double': [
+            '╭─────────────╮',
+            '│ ⚜ ⚜ ⚜ ⚜ ⚜ ⚜ │',
+            '│  ⚜ ⚜ ⚜ ⚜ ⚜  │',
+            '│ ⚜ ⚜ ⚜ ⚜ ⚜ ⚜ │',
+            '╰─────────────╯']},
+      'shoe card': {
+         'not_double': [
+            '▗▄▄▄▄▄▄▄▄▄',
+            '▐█████████',
+            '▐█████████',
+            '▐█████████',
+            '▐█████████',
+            '▐█████████',
+            '▝▀▀▀▀▀▀▀▀▀'],
+         'double': ['this card is not needed']}}
+   
+   if rank in card_graphics: #number Cards
+       if double:
+           return card_graphics[rank]['double']
+       else:
+           return card_graphics[rank]['not_double']
+   elif card in card_graphics: #Aces, Reverse, and Shoe Card
+       if double:
+           return card_graphics[card]['double']
+       else:
+           return card_graphics[card]['not_double']
+   elif rank in ('J','Q','K'):
+       if double:
+           return card_graphics['face']['double']
+       else:
+           return card_graphics['face']['not_double']
+   else: 
+       if double:
+           return [
+      '╭─────────────╮',
+      '│error        │',
+      '│        error│',
+      '│error        │',
+      '╰─────────────╯']   
+       else:
+           return [
+      '╭─────────╮',
+      '│error    │',
+      '│    error│',
+      '│error    │',
+      '│    error│',
+      '│error    │',
       '╰─────────╯']
-   if not double:
-      if rank in ('2', '3', '4', '5', '6', '7', '8', '9', '10'):
-         if rank == '2':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│2   {}    │'.format(suit),\
-            '│         │',\
-            '│         │',\
-            '│         │',\
-            '│    {}   2│'.format(suit),\
-            '╰─────────╯']
-         elif rank == '3':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│3   {}    │'.format(suit),\
-            '│         │',\
-            '│    {}    │'.format(suit),\
-            '│         │',\
-            '│    {}   3│'.format(suit),\
-            '╰─────────╯']
-         elif rank == '4':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│4 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│         │',\
-            '│         │',\
-            '│  {}   {} 4│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '5':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│5 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│    {}    │'.format(suit),\
-            '│         │',\
-            '│  {}   {} 5│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '6':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│6 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {} 6│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '7':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│7 {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {} 7│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '8':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│8 {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {} 8│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '9':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│9 {}   {}  │'.format(suit,suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│  {}   {} 9│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '10':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│10 {} {}   │'.format(suit,suit),\
-            '│  {} {} {}  │'.format(suit,suit,suit),\
-            '│         │',\
-            '│  {} {} {}  │'.format(suit,suit,suit),\
-            '│   {} {} 10│'.format(suit,suit),\
-            '╰─────────╯']
-      elif rank in ('J','Q','K'):
-         card_graphic = [\
-         '╭─────────╮',\
-         '│{}┌─────┐ │'.format(rank),\
-         '│{}│▓▓▒▒▓│ │'.format(suit),\
-         '│ │░░░░░│ │',\
-         '│ │▓▒▒▓▓│{}│'.format(suit),\
-         '│ └─────┘{}│'.format(rank),\
-         '╰─────────╯']
-      elif rank == 'A':
-         if card == 'A ♥':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A  _ _   │',\
-            '│♥ ( V )  │',\
-            '│   \ /   │',\
-            '│    V   ♥│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♦':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   ^    │',\
-            '│♦ /   \  │',\
-            '│  \   /  │',\
-            '│    v   ♦│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♠':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   .    │',\
-            '│♠  / \   │',\
-            '│  (_._)  │',\
-            '│    |   ♠│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♣':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   _    │',\
-            '│♣  ( )   │',\
-            '│  (_\'_)  │',\
-            '│    |   ♣│',\
-            '│        A│',\
-            '╰─────────╯']
-      elif card == 'back side':
-         card_graphic = [\
-         '╭─────────╮',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '│ ⚜ ⚜ ⚜ ⚜ │',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '│ ⚜ ⚜ ⚜ ⚜ │',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '╰─────────╯']
-      elif card == 'shoe card':
-         card_graphic = [\
-         '▗▄▄▄▄▄▄▄▄▄',\
-         '▐█████████',\
-         '▐█████████',\
-         '▐█████████',\
-         '▐█████████',\
-         '▐█████████',\
-         '▝▀▀▀▀▀▀▀▀▀']
-      return card_graphic
-   elif double:
-      if rank in ('2', '3', '4', '5', '6', '7', '8', '9', '10'):
-         if rank == '2':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│2   {}    │'.format(suit),\
-            '│         │',\
-            '│         │',\
-            '│         │',\
-            '│    {}   2│'.format(suit),\
-            '╰─────────╯']
-         elif rank == '3':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│3   {}    │'.format(suit),\
-            '│         │',\
-            '│    {}    │'.format(suit),\
-            '│         │',\
-            '│    {}   3│'.format(suit),\
-            '╰─────────╯']
-         elif rank == '4':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│4 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│         │',\
-            '│         │',\
-            '│  {}   {} 4│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '5':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│5 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│    {}    │'.format(suit),\
-            '│         │',\
-            '│  {}   {} 5│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '6':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│6 {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {} 6│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '7':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│7 {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│         │',\
-            '│  {}   {} 7│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '8':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│8 {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {} 8│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '9':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│9 {}   {}  │'.format(suit,suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│    {}    │'.format(suit),\
-            '│  {}   {}  │'.format(suit,suit),\
-            '│  {}   {} 9│'.format(suit,suit),\
-            '╰─────────╯']
-         elif rank == '10':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│10 {} {}   │'.format(suit,suit),\
-            '│  {} {} {}  │'.format(suit,suit,suit),\
-            '│         │',\
-            '│  {} {} {}  │'.format(suit,suit,suit),\
-            '│   {} {} 10│'.format(suit,suit),\
-            '╰─────────╯']
-      elif rank in ('J','Q','K'):
-         card_graphic = [\
-         '╭─────────╮',\
-         '│{}┌─────┐ │'.format(rank),\
-         '│{}│▓▓▒▒▓│ │'.format(suit),\
-         '│ │░░░░░│ │',\
-         '│ │▓▒▒▓▓│{}│'.format(suit),\
-         '│ └─────┘{}│'.format(rank),\
-         '╰─────────╯']
-      elif rank == 'A':
-         if card == 'A ♥':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A  _ _   │',\
-            '│♥ ( V )  │',\
-            '│   \ /   │',\
-            '│    V   ♥│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♦':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   ^    │',\
-            '│♦ /   \  │',\
-            '│  \   /  │',\
-            '│    v   ♦│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♠':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   .    │',\
-            '│♠  / \   │',\
-            '│  (_._)  │',\
-            '│    |   ♠│',\
-            '│        A│',\
-            '╰─────────╯']
-         elif card == 'A ♣':
-            card_graphic = [\
-            '╭─────────╮',\
-            '│A   _    │',\
-            '│♣  ( )   │',\
-            '│  (_\'_)  │',\
-            '│    |   ♣│',\
-            '│        A│',\
-            '╰─────────╯']
-      elif card == 'back side':
-         card_graphic = [\
-         '╭─────────╮',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '│ ⚜ ⚜ ⚜ ⚜ │',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '│ ⚜ ⚜ ⚜ ⚜ │',\
-         '│⚜ ⚜ ⚜ ⚜  │',\
-         '╰─────────╯']
-
-
+       
 deck = make_deck()
 example_hand = Hand()
+example_hand.deal_card(deck)
+example_hand.deal_card(deck)
 example_hand.deal_card(deck)
 example_hand.deal_card(deck)
 example_hand.deal_card(deck)
@@ -333,10 +309,14 @@ card1 = example_hand.cards[0]
 card2 = example_hand.cards[1]
 card3 = example_hand.cards[2]
 card4 = example_hand.cards[3]
+card5 = example_hand.cards[4]
+card6 = example_hand.cards[5]
 graphic1 = return_card(card1,False)
 graphic2 = return_card(card2,False)
 graphic3 = return_card(card3,False)
-graphic4 = return_card(card4,False)
+graphic4 = return_card(card4,True)
+graphic5 = return_card(card5,True)
+graphic6 = return_card(card6,True)
 for i in graphic1:
    print(i)
 for z in graphic2:
@@ -345,4 +325,7 @@ for n in graphic3:
    print(n)
 for x in graphic4:
    print(x)
-#Queens work, Kings work, Aces work
+for k in graphic5:
+   print(k)
+for j in graphic6:
+   print(j)
